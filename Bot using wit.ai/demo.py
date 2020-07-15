acb920093dc7fcbef3930b70ca48e5a6e9b6a5b8
Allen Carvallo <53426022+allencarvallo@users.noreply.github.com>
2020-07-16 00:00:01 +0530

    playing with wit and google speech recognition

--- Bot using wit.ai/demo.py
+++ Bot using wit.ai/demo.py
@@ -1,6 +1,46 @@
-from dotenv import load_dotenv
-load_dotenv()
-import os
+import speech_recognition as sr
+from wit import Wit
 
-server_access_token = os.getenv('SERVER_ACCESS_TOKEN')
-print(server_access_token)
+r = sr.Recognizer()
+
+access_token = "2YHFBJ6CMIGVD4V3PQPA4A6ZWTUYZHUF"
+
+def record_audio() :
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        voice_data = ''
+        try:
+            voice_data = r.recognize_google(audio)
+        except sr.UnknownValueError:
+            print('Sorry, I did not get that')
+        except sr.RequestError:
+            print("Sorry, speech service is down")
+        return voice_data
+
+
+print()
+print('Ralfe here, Say something....')
+voice_data = record_audio()
+print(voice_data)
+
+client = Wit(access_token)
+resp = client.message(voice_data)
+
+print()
+print('Wit.ai response')
+print()
+print(resp)
+print()
+
+intent = resp['intents'][0]['name']
+print("intent name : ",intent)
+print()
+confidence = resp['intents'][0]['confidence']
+print("confidence value : ",confidence)
+print()
+entity_value = resp['entities']['person:person'][0]['value']
+print("Entity name : ",entity_value)
+print()
+if intent == 'call_someone' and confidence >= 0.90 and entity_value == 'son' :
+    print("Calling Son....")
+    print()
