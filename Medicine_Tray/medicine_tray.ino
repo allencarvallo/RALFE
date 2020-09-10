be8cf3c8ccf17db695a63e1ad6b9ea1f999997fe
Allen Carvallo <53426022+allencarvallo@users.noreply.github.com>
2020-09-10 20:30:15 +0530

    Update medicine_tray.ino

--- Medicine_Tray/medicine_tray.ino
+++ Medicine_Tray/medicine_tray.ino
@@ -1,115 +1,124 @@
-#include <Servo.h> 
+#include <Servo.h>
+#include<SoftwareSerial.h>
 
 Servo myservo;
-  int sensor = 8;
-  int led = 3;
-  int buzzer = 8;
-  int flag=0;
-  int timesTosend=1;
-  char phone_no[]="7012805128";
-  int count=0;
 
+int IR=8;
+int led=12;
+
+int flag;
+char phone_no[] = "8086265488";
+int incomingByte = 0;
+
+SoftwareSerial mySerial(9, 10); // RX, TX  
 
 void setup() 
-{ 
-  myservo.attach(9);
-  pinMode(led, OUTPUT);
-  pinMode(sensor, INPUT);
-  delay(5000);
-  digitalWrite(sensor,LOW);
-  digitalWrite(led,LOW);
-  digitalWrite(buzzer,LOW);
-  Serial.begin(9600); 
-  Serial.println("AT+CMGF=1");    //set gsm to text mode
-}
-
-
-
-void loop() 
 {
-  myservo.write(0);  // set servo to mid-point
-  delay(10000); 
-  Serial.println("Time for morning medicine");
-  buzzer();
-  myservo.write(45);  // set servo to mid-point
-  check();
-  delay(3000);
-  myservo.write(0);  // set servo to mid-point
-  delay(10000); 
-  Serial.println("Time for afternoon medicine");
-  buzzer();
-  myservo.write(90);  // set servo to mid-point
-  check();
-  delay(3000);
-  myservo.write(0);  // set servo to mid-point
-  delay(10000); 
-  Serial.println("Time for evening medicine");
-  buzzer();
-  myservo.write(135);  // set servo to mid-point
-  check();
-  delay(3000);
-  myservo.write(0);  // set servo to mid-point
-  delay(10000); 
-  Serial.println("Time for night medicine");
-  buzzer();
-  myservo.write(180);  // set servo to mid-point
-  check();
-  delay(3000);
-  myservo.write(0);  // set servo to mid-point
-  delay(1000); 
-  } 
+  myservo.attach(7);
 
-  void buzzer()
-  {
-    digitalWrite(buzzer,HIGH);
-    delay(5000)
-    digitalWrite(buzzer,LOW);
-  }  
+  mySerial.begin(9600);   // Setting the baud rate of GSM Module  
+  Serial.begin(9600); // Setting the baud rate of Serial Monitor (Arduino)
+
+  pinMode(IR, INPUT);
+  pinMode(led, OUTPUT);
+
+  delay(2000);
+  Serial.println("AT+CMGF=1");   //set GSM to text mode
+  delay(200);
+}
+ void loop()
+ {
+   // send data only when you receive data:
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
   
-  void check()
-  { 
-    for(int i=0;i<30;i++) 
-    {
-      if(digitalRead(sensor)==HIGH)
-      {
-       Serial.println("high");
-       digitalWrite(led,HIGH);
-       flag=1;
-       break;
-      }
-      delay(2000);
-    }  
-    if(flag==0)
-    {
-      Serial.println("medicine not taken ");
-      Serial.println("preparing to send call......"); 
-      gsmcall();
-    }
-    else if(flag==1)
-    { 
-      Serial.println("medicine taken");
-      flag=0;
-    }
-  }
-  void gsmcall()
-  {
-    while(count<timesTosend)
-    {
-      delay(3000);
-      Serial.print("ATD");;
-      Serial.print(phone_no);
-      Serial.println(";");
-      delay(20000);
-      Serial.println("ATH");
-      delay(1000);
-      
-      Serial.print("AT+CMGS=\"");
-      Serial.print(phone_no);
-      Serial.println("\"");
-      delay(1500);
-      Serial.println("<<<<MEDICNE NOT TAKEN>>>>");
-      Serial.write(0x1A);               
-      delay(5000);
-      count++;
+    // say what you got:
+    Serial.print("I received: ");
+    Serial.println(incomingByte, DEC);
+    
+    if (incomingByte == 49) {
+     Serial.println("morning medicine");
+     delay(2000);
+     medicineTray(45);
      }
-   }                  
+     
+     if (incomingByte == 50) {
+      Serial.println("afternoon medicine");
+      delay(2000);
+      medicineTray(90);
+     }
+    
+      if (incomingByte == 51) {
+        Serial.println("night medicine");
+        delay(2000);
+        medicineTray(135);
+      }  
+ }
+ }
+
+void medicineTray(int x)
+{
+  myservo.write(0);
+   delay(1000);
+
+    myservo.write(x);
+    delay(1000);
+    
+    medicineTakenStatus(); 
+
+    myservo.write(0);
+
+  }
+
+  void medicineTakenStatus()
+  {
+   flag = 0;
+   for(int i= 0; i<20; i++)                       // waits for 20 sec
+   {  
+    Serial.println(i);
+    if(digitalRead(IR)==LOW)
+    {
+      digitalWrite(led, HIGH);
+      Serial.println("Motion Detected");
+      digitalWrite(led, LOW);
+      flag = 1;
+      break;
+      
+    }
+    delay(1000);
+   }
+    if (flag != 1)
+    { Serial.println("medicine not taken, make call to caretaker");
+    
+     gsmCall();
+     
+      }
+    }
+
+    void gsmCall()
+    {
+     if (mySerial.available()>0)
+      Serial.write(mySerial.read()); 
+     
+     delay(1000);
+     
+     Serial.println("dialing");
+     mySerial.print("ATD");;
+     mySerial.print(phone_no);
+     mySerial.println(";");
+     delay(20000);
+     mySerial.println("ATH");
+     delay(1000);
+     Serial.println("Call stopping");
+     
+     Serial.println("messaging");
+     mySerial.print("AT+CMGS=\"");
+     mySerial.print(phone_no);
+     mySerial.println("\"");
+     delay(1500); 
+     mySerial.print("CAUTION...INTRUDER ALERT ALARM !!!!!!!!!!"); 
+     mySerial.write(0x1A);
+     delay(2000);
+     flag = 0;  
+    }
